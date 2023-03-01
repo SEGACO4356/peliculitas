ab5ccf7ab128486e34fb51a46c4432724f0dbb22
Sebastián García Correa <sebastian435622@gmail.com>
2023-03-01 11:27:06 -0500

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import './App.css';
 const Home = lazy (()=>import("./Components/Home/Home"))
 const Login = lazy(()=>import("./Components/Login/Login"))
 const Registro = lazy(()=>import("./Components/Registro/Registro"))
+const Nav = lazy (()=> import("./Components/Nav/Nav"))
+const Error = lazy (()=>import("./Components/Error/Error"))
+
+
 
 
 function App() {
@@ -14,10 +18,12 @@ function App() {
   return (
 <>
     <Router>
+        <Nav />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/registro' element={<Registro />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='*' element={<Error />}></Route>
       </Routes>
     </Router>
 
