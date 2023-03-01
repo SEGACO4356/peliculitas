0aa0138ce4d4d91a24d8f2a3b1863a1ec0e6e65e
Sebastián García Correa <sebastian435622@gmail.com>
2023-03-01 14:53:39 -0500

    Xd

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import style from "./error.module.css";
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <>
+      <section className={style.section}>
+          <div className={style.container}>
+      <p className={style.p} ><span>Look like you're lost in space</span></p>
+      <h1 className={style.h1}>
+        <span>4</span>
+        <span>0</span>
+        <span>4</span>
+        </h1>
+      <a href="/" className={style.a} > <span>Go back</span></a>
+      </div>
+      </section>
+    </>
+  );
+};
 
-export default Error
+export default Error;
