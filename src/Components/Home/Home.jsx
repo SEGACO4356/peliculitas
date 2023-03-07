ca933d45ab923b2603275dd45630f6289536e311
Sebastián García Correa <sebastian435622@gmail.com>
2023-03-06 19:46:05 -0500

    Terminar map

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,26 +1,31 @@
-import React, {useState, useEffect} from 'react'
-import Loading from '../Loading/Loading'
+import React, { useState, useEffect } from "react";
+import Loading from "../Loading/Loading";
 import { getMovies } from "../../Services/axios.js";
 
 const Home = () => {
-
-  const [movies, setMovies] = useState(0)
-    useEffect(()=> {
-      const get = async () =>{
-        const res = await getMovies.getPopular();
-        setMovies(res)
-      }
-     get()
-    })
-    if(!movies){
-      return <Loading />
-    }
+  const [movies, setMovies] = useState(0);
+  useEffect(() => {
+    const get = async () => {
+      const res = await getMovies.getPopular();
+      setMovies(res);
+    };
+    get();
+  });
+  if (!movies) {
+    return <Loading />;
+  }
   return (
     <>
-
-    <div>{movies.data.results.map((k, index)=> <h2>{k.title}</h2>)}</div>
+      <div>
+        {movies.data.results.map((k, index) => (
+          <div key={index}>
+            <h2>{k.title}</h2>
+            <p>{k.overview}</p>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
