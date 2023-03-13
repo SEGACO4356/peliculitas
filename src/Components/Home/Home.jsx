b39d5f78a5b8757b43a78384f645bd17a8f924ef
Sebastián García Correa <sebastian435622@gmail.com>
2023-03-13 12:33:38 -0500

    Terminar home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import {useEffect, useState} from "react";
+import { getUpcoming } from "../../Services/axios";
 import style from "./home.module.css";
 import img from "../../images/Fondo.jpg";
-import img1 from "../../images/prueba.jpg"
-
-import { Swiper, SwiperSlide } from "swiper/react";
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y ,Autoplay} from 'swiper';
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import Loading from "../Loading/Loading";
 const Home = () => {
+  const [upcom, setUpcom] = useState (0);
+  useEffect(()=>{
+    const get = async () =>{
+      const res = await getUpcoming();
+      setUpcom(res);
+    } 
+    get();
+  })
+    if(!upcom){
+      return <Loading />
+    }
+
+
   return (
     <>
       <main className={style.main}>
@@ -20,25 +25,23 @@ const Home = () => {
             Here's your best site to watch Movies, Series, Tv programs and much
             more...
           </p>
-          <Swiper 
-            modules={[Navigation, Pagination, Scrollbar, A11y,Autoplay]}
-            spaceBetween={50}
-            slidesPerView={3}
-            navigation={true}
-            Autoplay={true}
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-          >
-            <SwiperSlide>
-              <img className={style.img} src={img} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img className={style.img} src={img1} alt="Carita feliz"/>
-            </SwiperSlide>
-          </Swiper>
+            <img className={style.img} src={img} alt="" />
         </div>
+   
+          
+          {upcom.data.results.map((k, index)=>(
+            
+            <div className={style.container_card}>
+              <p className={style.upcomming}>Upcomming</p>
+            <div className={style.card} key={index}>
+              <h2 className={style.title}>{k.title}</h2>
+              <img className={style.img1} src={`https://www.themoviedb.org/t/p/w220_and_h330_face${k.poster_path}`} alt="" />
+              <p className={style.overview}>{k.overview}</p>
+            </div>
+            </div>
+            
+           
+          ))}
       </main>
     </>
   );
--- src/Services/axios.js
+++ src/Services/axios.js
@@ -1,26 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
 
-   
+export const getPopular = async () => {
+  const data = await axios.get(
+    "https://api.themoviedb.org/3/movie/popular?api_key=a6f5e5862b0302f6a6f5f1bf82c782b8&language=en-US&page=1"
+  );
+  console.log(data);
+  return new Promise((res, rej) => {
+    res(data);
+  });
+};
 
- export const getPopular = async () =>{
-    const data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=a6f5e5862b0302f6a6f5f1bf82c782b8&language=en-US&page=1")
+export const getNow = async () => {
+  const data = await axios.get(
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=a6f5e5862b0302f6a6f5f1bf82c782b8&language=en-US&page=1"
+  );
+  console.log(data);
+  return data;
+};
+export const getTop = async () => {
+  const data = await axios.get(
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=a6f5e5862b0302f6a6f5f1bf82c782b8&language=en-US&page=1"
+  );
+  console.log(data);
+  return data;
+};
+export const getUpcoming = async () =>{
+    const data = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=a6f5e5862b0302f6a6f5f1bf82c782b8&language=en-US&page=1")
     console.log(data);
-    return new Promise( (res, rej) => {
-        res(data);
-    });
+    return data;
 }
 
- export const getNow = async () =>{
-    const data = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=a6f5e5862b0302f6a6f5f1bf82c782b8&language=en-US&page=1")
-    console.log(data);
-    return(
-        data
-    );
-    }
-    export const getTop = async ()=>{
-        const data = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=a6f5e5862b0302f6a6f5f1bf82c782b8&language=en-US&page=1")
-        console.log(data);
-        return(
-            data
-        )
-}
